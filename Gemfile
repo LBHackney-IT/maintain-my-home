9dbe41751f6d94d1b163a007c6667692afd4374a
Robert Lee-Cann <leeky@leeky.org.uk>
2017-08-31 11:44:24 +0100

    (chore) Add Rubocop with basic rules
    Also, fix any current rule violations.

--- Gemfile
+++ Gemfile
@@ -1,32 +1,33 @@
 source 'https://rubygems.org'
 
 git_source(:github) do |repo_name|
-  repo_name = "#{repo_name}/#{repo_name}" unless repo_name.include?("/")
+  repo_name = "#{repo_name}/#{repo_name}" unless repo_name.include?('/')
   "https://github.com/#{repo_name}.git"
 end
 
-gem 'rails', '~> 5.1.3'
-gem 'pg', '~> 0.18'
-gem 'puma', '~> 3.7'
-gem 'sass-rails', '~> 5.0'
-gem 'uglifier', '>= 1.3.0'
 gem 'dotenv-rails'
 gem 'faker' # Use in all environments until we have a real API
+gem 'pg', '~> 0.18'
+gem 'puma', '~> 3.7'
+gem 'rails', '~> 5.1.3'
+gem 'sass-rails', '~> 5.0'
+gem 'uglifier', '>= 1.3.0'
 
 group :development, :test do
+  gem 'factory_girl_rails'
   gem 'pry-rails'
   gem 'rspec-rails'
-  gem 'factory_girl_rails'
+  gem 'rubocop', '~> 0.49.1' # Locked because minor versions might change cops
 end
 
 group :development do
-  gem 'web-console', '>= 3.3.0'
   gem 'listen', '>= 3.0.5', '< 3.2'
+  gem 'web-console', '>= 3.3.0'
 end
 
 group :test do
-  gem 'webmock'
   gem 'simplecov', require: false
+  gem 'webmock'
 end
 
 group :production do
--- config/application.rb
+++ config/application.rb
@@ -1,15 +1,15 @@
 require_relative 'boot'
 
-require "rails"
+require 'rails'
 # Pick the frameworks you want:
-require "active_model/railtie"
-require "active_job/railtie"
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_view/railtie"
+require 'active_model/railtie'
+require 'active_job/railtie'
+require 'active_record/railtie'
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'action_view/railtie'
 # require "action_cable/engine"
-require "sprockets/railtie"
+require 'sprockets/railtie'
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
@@ -21,9 +21,10 @@ module HackneyRepairs
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # Settings in config/environments/* take precedence over those specified
+    # here. Application configuration should go into files in
+    # config/initializers -- all .rb files in that directory are
+    # automatically loaded.
 
     # Don't generate system test files.
     config.generators.system_tests = nil
