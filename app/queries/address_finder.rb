68ad4253f669d4bb6609138b3522d0690f73ce7b
Duncan Stuart <dgmstuart@gmail.com>
2017-09-07 17:11:32 +0100

    (refactor) AddressFinder returns objects
    This is perhaps a trade-off:
    - Address finder isn't building a structure specifically for views (an
    array of arrays)
    - The view needs to know the names of the data attributes it's
    displaying
      - On the other hand this feels easier to understand than :first/:last

--- app/queries/address_finder.rb
+++ app/queries/address_finder.rb
@@ -1,5 +1,9 @@
 class AddressFinder
   def find(_form)
-    [['P01234', 'Flat 1, 8 Hoxton Square, N1 6NU']]
+    [
+      Result.new('P01234', 'Flat 1, 8 Hoxton Square, N1 6NU'),
+    ]
   end
+
+  Result = Struct.new(:property_reference, :short_address)
 end
--- spec/queries/address_finder_spec.rb
+++ spec/queries/address_finder_spec.rb
@@ -4,7 +4,9 @@ require 'app/queries/address_finder'
 RSpec.describe AddressFinder do
   describe '.find' do
     it 'returns some hardcoded data' do
-      expect(AddressFinder.new.find(double)).to eq [['P01234', 'Flat 1, 8 Hoxton Square, N1 6NU']]
+      result = AddressFinder.new.find(double)
+      expect(result.first.property_reference).to eq 'P01234'
+      expect(result.first.short_address).to eq 'Flat 1, 8 Hoxton Square, N1 6NU'
     end
   end
 end
