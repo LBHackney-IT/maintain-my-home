477bf1e0f2d5a2a8e43a9527572561fabf82941a
Duncan Stuart <dgmstuart@gmail.com>
2017-09-07 16:39:39 +0100

    (fix) move simplecov config to rails_helper
    We only care about test coverage when running the whole suite, and
    specifically the SimpleCov 'rails' config only makes sense when running
    all the tests.
    
    Also, tell SimpleCov about queries, so that they are shown in their own
    tab in the output, rather than in 'undefined'.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,4 +1,8 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
+require 'simplecov'
+SimpleCov.start 'rails' do
+  add_group 'Queries', 'app/queries'
+end
+
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
