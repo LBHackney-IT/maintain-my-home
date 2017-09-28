4fceedb84f62c01945717f4f4e0a68ceea0fbb86
Duncan Stuart <dgmstuart@gmail.com>
2017-09-28 18:19:43 +0100

    (feature) Non-JSON responses raise an exception
    We'll want the consumers of this class to be confident that they're
    getting JSON, and we don't necessarily want to require the API to
    specify an application/json content type (right?).
    
    Therefore, assume what we're getting is JSON, and raise an exception if
    not.
    
    - Re-raise the Faraday parsing errors so that users of this class aren't
      coupled to Faraday.
    - Check that we still handle empty bodies correctly.

--- app/queries/json_api.rb
+++ app/queries/json_api.rb
@@ -1,16 +1,20 @@
 class JsonApi
+  class Error < StandardError; end
   class InvalidApiRootError < StandardError; end
+  class InvalidResponseError < StandardError; end
 
   def initialize(api_root: ENV['HACKNEY_API_ROOT'])
     raise InvalidApiRootError, 'api_root was nil or empty' if api_root.blank?
 
     @connection = Faraday.new api_root do |conn|
       conn.adapter Faraday.default_adapter
-      conn.response :json, content_type: /\bjson$/
+      conn.response :json
     end
   end
 
   def get(path)
     @connection.get(path).body
+  rescue Faraday::ParsingError => e
+    raise InvalidResponseError, e.message
   end
 end
--- spec/queries/json_api_spec.rb
+++ spec/queries/json_api_spec.rb
@@ -18,6 +18,38 @@ RSpec.describe JsonApi do
 
         expect(result).to eq [{ 'property_reference' => 'abc123', 'short_address' => '1 some road' }]
       end
+
+      it 'parses a JSON response with an unspecified content_type' do
+        json_api = JsonApi.new(api_root: 'http://hackney.api:8000')
+        stub_request(:get, 'http://hackney.api:8000/properties?postcode=A1%201AA')
+          .to_return(
+            body: [{ 'property_reference' => 'abc123', 'short_address' => '1 some road' }].to_json
+          )
+
+        result = json_api.get('properties?postcode=A1 1AA')
+
+        expect(result).to eq [{ 'property_reference' => 'abc123', 'short_address' => '1 some road' }]
+      end
+
+      it 'raises an exception for an invalid json response' do
+        json_api = JsonApi.new(api_root: 'http://hackney.api:8000')
+        stub_request(:get, 'http://hackney.api:8000/properties?postcode=A1%201AA')
+          .to_return(
+            body: '<html><body>An error occurred</body></html>'
+          )
+
+        expect { json_api.get('properties?postcode=A1 1AA') }
+          .to raise_error(JsonApi::InvalidResponseError, "765: unexpected token at '<html><body>An error occurred</body></html>'")
+      end
+
+      it 'handles an empty response body' do
+        json_api = JsonApi.new(api_root: 'http://hackney.api:8000')
+        stub_request(:get, 'http://hackney.api:8000/properties?postcode=A1%201AA')
+
+        result = json_api.get('properties?postcode=A1 1AA')
+
+        expect(result).to eq nil
+      end
     end
 
     context 'with a path matching properties/:property_reference' do
