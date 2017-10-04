6b8125d09222daa5757350203f08b4fabf2ad81f
Duncan Stuart <dgmstuart@gmail.com>
2017-10-04 14:38:33 +0100

    (spec) Failing: confirmation page (Varying data)
    This is to force us to now test-drive out the actual functionality by
    changing the data to be different from the hardcoded values.

--- spec/features/residents_can_see_confirmation_of_repair_request_spec.rb
+++ spec/features/residents_can_see_confirmation_of_repair_request_spec.rb
@@ -4,7 +4,7 @@ RSpec.feature 'Resident can see a confirmation of their repair request' do
   scenario 'when the repair request creation was successful' do
     property = {
       'property_reference' => '00000503',
-      'short_address' => 'Ross Court 30',
+      'short_address' => 'Ross Court 23',
       'postcode' => 'E5 8TE',
     }
     fake_api = instance_double(JsonApi)
@@ -20,7 +20,7 @@ RSpec.feature 'Resident can see a confirmation of their repair request' do
     click_on 'Continue'
 
     # Describe problem:
-    fill_in 'dummy_form_description', with: "The bath won't drain"
+    fill_in 'dummy_form_description', with: 'My sink is blocked'
     click_on 'Continue'
 
     # Address search:
@@ -28,23 +28,25 @@ RSpec.feature 'Resident can see a confirmation of their repair request' do
     click_on 'Find my address'
 
     # Address selection:
-    choose_radio_button 'Ross Court 30'
+    choose_radio_button 'Ross Court 23'
     click_on 'Continue'
 
     # Contact details - last page before confirmation:
-    fill_in 'Full name', with: 'John Smith'
-    fill_in 'Telephone number', with: '01234567890'
-    check 'afternoon (12pm - 5pm)'
+    fill_in 'Full name', with: 'John Evans'
+    fill_in 'Telephone number', with: '078 98765 432'
+    check 'morning (8am - 12pm)'
     click_on 'Continue'
 
-    expect(page).to have_content 'Your reference number is abc123'
+    aggregate_failures do
+      expect(page).to have_content 'Your reference number is abc123'
 
-    expect(page).to have_content t('confirmation.summary.description', description: "The bath won't drain")
+      expect(page).to have_content t('confirmation.summary.description', description: 'My sink is blocked')
 
-    expect(page).to have_content t('confirmation.summary.name', name: 'John Smith')
-    expect(page).to have_content t('confirmation.summary.phone', phone: '01234 567 890')
-    expect(page).to have_content t('confirmation.summary.address', address: 'Ross Court 30, E5 8TE')
+      expect(page).to have_content t('confirmation.summary.name', name: 'John Evans')
+      expect(page).to have_content t('confirmation.summary.phone', phone: '07898765432')
+      expect(page).to have_content t('confirmation.summary.address', address: 'Ross Court 23, E5 8TE')
 
-    expect(page).to have_content t('confirmation.summary.time', time: 'afternoon (12pm - 5pm)')
+      expect(page).to have_content t('confirmation.summary.time', time: 'morning (8am - 12pm)')
+    end
   end
 end
