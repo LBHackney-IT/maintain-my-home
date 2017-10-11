57110315f222b51b4a17d4f35ba1843ee6bb938a
Robert Lee-Cann <leeky@leeky.org.uk>
2017-10-11 13:51:14 +0100

    (chore) Rename service to "Report a repair"

--- spec/features/users_understand_purpose_of_service_spec.rb
+++ spec/features/users_understand_purpose_of_service_spec.rb
@@ -4,7 +4,7 @@ RSpec.feature 'Users understand the purpose of the service' do
   scenario 'sees the start page when visiting the root path' do
     visit '/'
 
-    expect(page).to have_content 'Maintain my home'
+    expect(page).to have_content 'Report a repair'
     expect(page).to have_link 'Start'
   end
 
