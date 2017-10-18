e9504887dea461e7530e3e59a85cdad12b7b019f
Duncan Stuart <dgmstuart@gmail.com>
2017-10-18 18:35:57 +0100

    (refactor) Extract a ContactDetailsForm
    ...and define ContactDetailsWithCallbackForm in terms of it.
    
    I'd normally prefer to use Decoration rather than inheritance here, but
    with ActiveModel objects that becomes fiddly where errors are concerned:
    
    Because we will want to call e.g. `form.errors`, the errors need to be all on the
    same object, but when using decoration you would e.g. have the contact
    details errors on the decorated ContactDetailsForm, and only the
    callback time errors will be on the ContactDetailsWithCallbackForm.
    
    To get around that you end up having to get any errors on the decorated
    object and reapply them onto the decorator. This quickly gets
    complicated.
    
    As long as we don't end up needing
    ContactDetailsWithCallbackAndSomethingElseForm then hopefully
    inheritance should be fine for us.

--- app/models/contact_details_form.rb
+++ app/models/contact_details_form.rb
@@ -1,13 +1,12 @@
 class ContactDetailsForm
   include ActiveModel::Model
 
-  attr_reader :full_name, :telephone_number, :callback_time
+  attr_reader :full_name, :telephone_number
 
-  validates :full_name, :telephone_number, :callback_time, presence: true
+  validates :full_name, :telephone_number, presence: true
 
   def initialize(hash = {})
     @full_name = hash[:full_name]
     @telephone_number = hash[:telephone_number]
-    @callback_time = hash[:callback_time]
   end
 end
