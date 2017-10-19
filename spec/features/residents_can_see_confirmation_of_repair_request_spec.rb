065e72b163905ba31995ff67606a25b445f82c1b
Duncan Stuart <dgmstuart@gmail.com>
2017-10-19 10:32:15 +0100

    (spec) Diagnosed repairs don't have a callback
    Remove the callback-related parts of the spec. In a later story we'll
    add in steps for booking appointments.
    
    - use the workOrderReference as the id for the page. This is potentially
      confusing, but seems like the most appropriate ID to use for this
      branch. Ultimately we'll hopefully have some other kind of more
      user-friendly reference.

--- spec/features/residents_can_see_confirmation_of_repair_request_spec.rb
+++ spec/features/residents_can_see_confirmation_of_repair_request_spec.rb
@@ -14,6 +14,7 @@ RSpec.feature 'Resident can see a confirmation of their repair request' do
       .with('repairs', anything)
       .and_return(
         'requestReference' => '00367923',
+        'workOrderReference' => '09124578',
         'priority' => 'N',
         'problem' => 'My sink is blocked',
         'propertyRef' => '00000503',
@@ -77,13 +78,11 @@ RSpec.feature 'Resident can see a confirmation of their repair request' do
     # Contact details - last page before confirmation:
     fill_in 'Full name', with: 'John Evans'
     fill_in 'Telephone number', with: '078 98765 432'
-    check 'morning (8am - 12pm)'
     click_on 'Continue'
 
     aggregate_failures do
       within '#confirmation' do
-        expect(page).to have_content 'Your reference number is 00367923'
-        expect(page).to have_content 'between 8am and 12pm'
+        expect(page).to have_content 'Your reference number is 09124578'
       end
 
       within '#summary' do
@@ -92,8 +91,6 @@ RSpec.feature 'Resident can see a confirmation of their repair request' do
         expect(page).to have_content t('confirmation.summary.name', name: 'John Evans')
         expect(page).to have_content t('confirmation.summary.phone', phone: '07898765432')
         expect(page).to have_content t('confirmation.summary.address', address: 'Ross Court 23, E5 8TE')
-
-        expect(page).to have_content t('confirmation.summary.callback_time', callback_time: 'between 8am and 12pm')
       end
     end
 
