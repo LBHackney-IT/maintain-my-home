89030f99808d2d63c51f5465cc9122684dc7b2a6
Duncan Stuart <dgmstuart@gmail.com>
2017-10-20 11:03:54 +0100

    (feature) Contact details form is validated
    Largely the same as the ContactDetailsWithCallback controller.

--- app/controllers/contact_details_controller.rb
+++ app/controllers/contact_details_controller.rb
@@ -10,11 +10,15 @@ class ContactDetailsController < ApplicationController
 
     contact_details_saver =
       ContactDetailsSaver.new(selected_answer_store: selected_answer_store)
-    contact_details_saver.save(@form)
-    result =
-      CreateRepair.new.call(answers: selected_answer_store.selected_answers)
 
-    redirect_to confirmation_path(result.work_order_reference)
+    if contact_details_saver.save(@form)
+      result =
+        CreateRepair.new.call(answers: selected_answer_store.selected_answers)
+
+      redirect_to confirmation_path(result.work_order_reference)
+    else
+      render :index
+    end
   end
 
   private
