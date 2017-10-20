6c9ada51eba8b3e64d16219e748b1b49facd25b1
Duncan Stuart <dgmstuart@gmail.com>
2017-10-20 11:13:03 +0100

    (feature) User can go back from confirmation page

--- spec/features/residents_can_navigate_back_spec.rb
+++ spec/features/residents_can_navigate_back_spec.rb
@@ -18,7 +18,63 @@ RSpec.feature 'Resident can navigate back' do
     allow(QuestionSet).to receive(:new).and_return(fake_question_set)
   end
 
-  scenario 'taking a full happy path through the forms' do
+  scenario 'when the repair was diagnosed' do
+    property = {
+      'property_reference' => 'abc123',
+      'short_address' => 'Flat 1, 8 Hoxton Square, N1 6NU',
+    }
+    fake_api = instance_double(JsonApi)
+    allow(fake_api).to receive(:get).with('properties?postcode=E8 5TQ').and_return([property])
+    allow(fake_api).to receive(:get).with('properties/abc123').and_return(property)
+    allow(JsonApi).to receive(:new).and_return(fake_api)
+
+    fake_question_set = instance_double(QuestionSet)
+    allow(fake_question_set)
+      .to receive(:find)
+      .and_return(
+        Question.new(
+          'question' => 'Dummy question',
+          'answers' => [
+            { 'text' => 'diagnose', 'sor_code' => '12345678' },
+          ],
+        )
+      )
+    allow(QuestionSet).to receive(:new).and_return(fake_question_set)
+
+    visit '/'
+    click_on 'Start'
+
+    # Emergency page:
+    choose_radio_button 'No'
+    click_on 'Continue'
+
+    # Fake decision tree
+    choose_radio_button 'diagnose'
+    click_continue
+
+    # Describe repair
+    click_continue
+
+    # Address search:
+    fill_in 'Postcode', with: 'E8 5TQ'
+    click_on 'Find my address'
+
+    # Address selection:
+    choose_radio_button 'Flat 1, 8 Hoxton Square, N1 6NU'
+    click_on 'Continue'
+
+    # Contact details
+    click_on t('back_links.address_searches')
+    expect(page).to have_content 'What is your address?'
+
+    click_on t('back_links.describe_repair')
+    expect(page).to have_content 'Is there anything else we should know?'
+
+    click_on t('back_links.questions/start')
+    expect(page).to have_content 'Do any of the following apply to you:'
+  end
+
+  scenario 'when the repair could not be diagnosed' do
     property = {
       'property_reference' => 'abc123',
       'short_address' => 'Flat 1, 8 Hoxton Square, N1 6NU',
@@ -50,7 +106,7 @@ RSpec.feature 'Resident can navigate back' do
     choose_radio_button 'Flat 1, 8 Hoxton Square, N1 6NU'
     click_on 'Continue'
 
-    # Contact details - last page before confirmation:
+    # Contact details with callback - last page before confirmation:
     click_on t('back_links.address_searches')
     expect(page).to have_content 'What is your address?'
 
@@ -84,7 +140,59 @@ RSpec.feature 'Resident can navigate back' do
     expect(page).to have_content 'Is there anything else we should know?'
   end
 
-  scenario 'when the contact details search was invalid' do
+  scenario 'when the contact details values were invalid' do
+    property = {
+      'property_reference' => 'abc123',
+      'short_address' => 'Flat 1, 8 Hoxton Square, N1 6NU',
+    }
+    fake_api = instance_double(JsonApi)
+    allow(fake_api).to receive(:get).with('properties?postcode=E8 5TQ').and_return([property])
+    allow(fake_api).to receive(:get).with('properties/abc123').and_return(property)
+    allow(JsonApi).to receive(:new).and_return(fake_api)
+
+    fake_question_set = instance_double(QuestionSet)
+    allow(fake_question_set)
+      .to receive(:find)
+      .and_return(
+        Question.new(
+          'question' => 'Dummy question',
+          'answers' => [
+            { 'text' => 'diagnose', 'sor_code' => '12345678' },
+          ],
+        )
+      )
+    allow(QuestionSet).to receive(:new).and_return(fake_question_set)
+
+    visit '/'
+    click_on 'Start'
+
+    # Emergency page:
+    choose_radio_button 'No'
+    click_on 'Continue'
+
+    # Fake decision tree
+    choose_radio_button 'diagnose'
+    click_continue
+
+    # Describe repair
+    click_continue
+
+    # Address search:
+    fill_in 'Postcode', with: 'E8 5TQ'
+    click_on 'Find my address'
+
+    # Address selection:
+    choose_radio_button 'Flat 1, 8 Hoxton Square, N1 6NU'
+    click_on 'Continue'
+
+    # Contact details - submit an empty form
+    click_on 'Continue'
+
+    click_on t('back_links.address_searches')
+    expect(page).to have_content 'What is your address?'
+  end
+
+  scenario 'when the contact details with callback values were invalid' do
     property = {
       'property_reference' => 'abc123',
       'short_address' => 'Flat 1, 8 Hoxton Square, N1 6NU',
