7a26ee06ee748c1fc9711efd0460732170adccb1
Duncan Stuart <dgmstuart@gmail.com>
2017-11-24 14:00:05 +0000

    (refactor/fix) Rationalise all phone numbers
    There are a lot of pages in the site which include phone numbers. In
    some cases the numbers were inconsistent, and how they were presented on
    the page was inconsistent as well.
    
    Now there is only one place where phone numbers are explicitly written,
    and everywhere they are used in the pages they are used in the same way:
    as a telephone number link with the appropriate 'itemprop'.
    
    Note that in some places the specific number used on a page has been
    corrected to the most appropriate one.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,35 @@ module ApplicationHelper
   def back_link
     tag.span class: 'js-back-link', data: { text: t('back_link') }
   end
+
+  def telephone_number(number)
+    tag.span itemprop: 'telephone' do
+      link_to number, "tel:#{number.gsub!(/\s+/, '')}"
+    end
+  end
+
+  def repairs_contact_centre_telephone_number
+    telephone_number('020 8356 3691')
+  end
+
+  def repairs_emergency_telephone_number
+    # NOTE: This is the same as the main RCC number
+    # but redirects to another number out of hours
+    telephone_number('020 8356 3691')
+  end
+
+  def gas_emergency_telephone_number
+    telephone_number('0800 111 999')
+  end
+
+  def electricity_emergency_telephone_number
+    # TODO: this is the 'temporary emergency number'
+    # should possibly be the national power network
+    # number instead? '0800 028 0247'
+    telephone_number('020 8356 2300')
+  end
+
+  def water_emergency_telephone_number
+    telephone_number('0800 714 614')
+  end
 end
---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe ApplicationHelper do
+  describe '#telephone_number' do
+    it 'wraps the phone number in appropriate markup' do
+      expect(helper.telephone_number('01234 567 890'))
+        .to eq(%(<span itemprop="telephone"><a href="tel:01234567890">01234567890</a></span>))
+    end
+  end
+end
